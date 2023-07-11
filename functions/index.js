1e7f2541e8d5c2b6ce90ecf62c47a7e45891bcce
Max Espinosa <maxespinosa@Maxs-MacBook-Air.local>
2023-07-11 13:49:08 -0400

    wip added update function CRUD

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import { onRequest } from "firebase-functions/v2/https";
 import express from "express"
 import cors from "cors"
-import { getTasks, addTasks} from "./src/tasks.js";
+import { getTasks, addTasks, updateTask} from "./src/tasks.js";
 
 const app = express();
 app.use(cors());
@@ -10,5 +10,6 @@ app.use(express.json());
 //routes:
 app.get("/tasks/:uid", getTasks);
 app.post("/tasks/:uid", addTasks);
+app.patch("/tasks/:uid", updateTask);
 
 export const api = onRequest({maxInstances: 10},app) //exports cloud function
